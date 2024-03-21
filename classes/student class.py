5bab4578c05d86f0ec6ddc7a3ca23e4517e931a1
edcos12 <163158090+edcos12@users.noreply.github.com>
2024-03-21 10:05:21 +0000

    Update student class.py
    made changes to student class.py file

--- classes/student class.py
+++ classes/student class.py
@@ -1,8 +1,11 @@
 class student:
-    def __init__(self, age, name, class_num):
+    def __init__(self, age, name, class_num, score1, score2, score3):
         self.age = age
         self.name = name
         self.class_num = class_num
+        self.score1 = score1 
+        self.score2 = score2
+        self.score3 = score3 
 
     def set_age(self):
         self.age = input("age: ")
@@ -14,31 +17,29 @@ class student:
         self.class_num = input("class number: ")
 
     def set_grades(self):
-        score1 = int(input("grade 1: "))
-        score2 = int(input("grade 2: "))
-        score3 = int(input("grade 3: "))
+        self.score1 = int(input("grade 1: "))
+        self.score2 = int(input("grade 2: "))
+        self.score3 = int(input("grade 3: "))
 
-        self.avg = round(score1 + score2 + score3 / 3, 2)
+    def create_student(self):
+        self.set_name(self)
+        self.set_age(self)
+        self.set_class_num(self)
+        self.set_grades(self)
 
     def get_average_score(self):
-        return "average grade: " + str(self.avg) 
+        self.avg = round((self.score1 + self.score2 + self.score3) / 3, 2)
+        return "Average grade: " + str(self.avg) 
 
     def get_age(self):
-        return "age: " + str(self.age)
+        return "Age: " + str(self.age)
     
     def get_name(self):
-        return "name: " + self.name
+        return "Name: " + self.name
     
     def get_class_num(self):
-        return "class: " + self.class_num
+        return "Class: " + self.class_num
     
-student1 = student
-student.set_name(student1)
-student.set_age(student1)
-student.set_class_num(student1)
-student.set_grades(student1)
-print("student1: ")
-print(student.get_name(student1))
-print(student.get_age(student1))
-print(student.get_class_num(student1))
-print(student.get_average_score(student1))
+student1 = student("19", "Edward", "4B", 6, 5, 8 )
+#student.create_student(student1)
+print(f"Student1: \n {student.get_name(student1)} \n {student.get_age(student1)} \n {student.get_class_num(student1)} \n {student.get_average_score(student1)} ")
